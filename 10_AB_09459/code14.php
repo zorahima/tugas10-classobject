89f122885d981fbc200119683848b858c182501f
zorahima <zorahima10@gmail.com>
2017-09-24 22:37:09 +0700

    Code 14 update

--- 10_AB_09459/code14.php
+++ 10_AB_09459/code14.php
@@ -1,17 +1,17 @@
 <?php
 abstract class smartphone{
 // buat abstract method
-abstract public function lihat_spec();
+	abstract public function lihat_spec();
 }
 class tablet extends smartphone{
 // implementasi abstract method
-public function lihat_spec(){
-return "Lihat Spec Tablet...";
-}
+	public function lihat_spec(){
+		return "Lihat Spec Tablet...";
+	}
 // method 'biasa'
-public function beli_tablet(){
-return "Beli Tablet...";
-}
+	public function beli_tablet(){
+		return "Beli Tablet...";
+	}
 }
 $tablet_obj = new tablet();
 echo $tablet_obj->lihat_spec();
